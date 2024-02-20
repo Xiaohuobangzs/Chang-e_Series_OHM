e3bbd199c50f7d82f5cc9f3630c93a6015f8063a
Guoyu <101939100+Xiaohuobangzs@users.noreply.github.com>
2024-02-20 13:28:58 +0800

    更新 240220 BETA1.0.0
    使文件路径清晰明了，加入登陆页面并外部引入js（登录逻辑判断），优化部分css

---
+++ js/n.js
@@ -0,0 +1,15 @@
+document.getElementById('loginForm').onsubmit = function(event) {
+  event.preventDefault();
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  if(username == 'betauser' && password == 'neice_ohm123') {
+    // 账号密码正确后弹窗
+	alert('登录成功！正在重定向到嫦娥一号介绍页......');
+	// 登录成功后设置标志
+    localStorage.setItem('isLoggedIn', 'true');
+    // 登录成功后跳转到指定页面
+    window.location.href = './cexl/change1.html';
+  } else {
+    alert('账号或密码错误！');
+  }
+};
