217713f75d8fd1af830cbff6dea23abe72aca43f
Geektext <101939100+Xiaohuobangzs@users.noreply.github.com>
2024-02-23 22:00:13 +0800

    BETA 1.1.0版本更新
    修复了部分情况点击button无法跳转的问题，改进了button的css美化

--- js/n.js
+++ js/n.js
@@ -2,11 +2,11 @@ document.getElementById('loginForm').onsubmit = function(event) {
   event.preventDefault();
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
-  if(username == 'betauser' && password == 'neice_ohm123') {
+  if(username == 'betauser' && password == 'neice_ohm123' || username == 'h' && password == 'hhpwd123') {
     // 账号密码正确后弹窗
-	alert('登录成功！正在重定向到嫦娥一号介绍页......');
-	// 登录成功后设置标志
-    localStorage.setItem('isLoggedIn', 'true');
+	alert('用户'+username+'您好！登录成功！请注意：您的账号是内测账号。正在重定向到嫦娥一号介绍页......');
+	// 登录成功后设置标志 
+	localStorage.setItem('isLoggedIn', 'true');
     // 登录成功后跳转到指定页面
     window.location.href = './cexl/change1.html';
   } else {
